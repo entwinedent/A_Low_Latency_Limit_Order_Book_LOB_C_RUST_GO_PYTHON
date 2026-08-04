d308b1a663c4b2938a2a323cdbcebeb5c7c9776a
Jayson Maxfield <jaysonmaxfield@gmail.com>
2026-08-03 20:40:25 -0400

    Fix Kokkos mdspan to only load on Linux (Docker environment)

--- core/include/lob/Common.h
+++ core/include/lob/Common.h
@@ -21,7 +21,7 @@
 
 #ifdef HAVE_STD_MDSPAN
 #include <mdspan>
-#elif defined(HAVE_KOKKOS_MDSPAN)
+#elif defined(HAVE_KOKKOS_MDSPAN) && defined(__linux__)
 #include <mdspan/mdspan.hpp>
 namespace std {
     // Alias Kokkos mdspan to std namespace for compatibility
